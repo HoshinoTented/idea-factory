33ed782f284c3677a2acad97b37bae5e3599d0d2
HoshinoTented <hoshinotented@qq.com>
2025-05-19 19:45:22 +0800

    signature: properly erasing

--- classfile/src/main/kotlin/org/aya/tool/classfile/sig-helper.kt
+++ classfile/src/main/kotlin/org/aya/tool/classfile/sig-helper.kt
@@ -4,6 +4,7 @@ import java.lang.classfile.MethodSignature
 import java.lang.classfile.Signature
 import java.lang.constant.ClassDesc
 import kala.collection.mutable.MutableList
+import kala.tuple.primitive.IntObjTuple2
 import java.lang.constant.ConstantDescs
 import java.lang.constant.MethodTypeDesc
 
@@ -51,14 +52,27 @@ inline fun buildSignature(builder: SignatureBuilder.() -> Unit): SignatureBuilde
 fun Signature.erase(): ClassDesc {
   return when (this) {
     is Signature.BaseTypeSig -> ClassDesc.of(this.signatureString())
-    is Signature.ArrayTypeSig -> componentSignature().erase().arrayType(arrayDepth())
+    is Signature.ArrayTypeSig -> {
+      val decomposed = this.decompose()
+      val depth = decomposed.component1
+      val elementType = decomposed.component2
+      elementType.erase().arrayType(depth)
+    }
     is Signature.ClassTypeSig -> ClassDesc.ofInternalName(className())
     is Signature.TypeVarSig -> ConstantDescs.CD_Object
   }
 }
 
-fun Signature.ArrayTypeSig.arrayDepth(): Int {
-  return this.signatureString().takeWhile { it != '[' }.length
+fun Signature.ArrayTypeSig.decompose(): IntObjTuple2<Signature> {
+  var depth = 0
+  var sig: Signature = this
+  
+  while (sig is Signature.ArrayTypeSig) {
+    depth += 1
+    sig = sig.componentSignature()
+  }
+  
+  return IntObjTuple2.of(depth, sig)
 }
 
 fun MethodSignature.erase(): MethodTypeDesc {
